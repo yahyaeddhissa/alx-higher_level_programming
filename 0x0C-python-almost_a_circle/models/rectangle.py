18fcc80c825f10e580d3c25aaff2b553d1b4821e
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-01-16 03:04:14 -0800

    String representation of a rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,9 @@ class Rectangle(Base):
             [print("#", end="") for w in range(self.width)]
             print("")
             
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
+    
